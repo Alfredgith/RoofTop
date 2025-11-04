fb67e155cda651d3ae646cef7887f4a23f5498a1
lenge <lengealfred@gmail.com>
2025-11-04 04:49:27 +0100

    gunicorn edit

--- mypro/mypro/settings.py
+++ mypro/mypro/settings.py
@@ -138,6 +138,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOWED_ORIGINS = [
     'http://10.223.88.179:8000',
     'http://localhost:8000',
+    'https://roof4.onrender.com'
     
     
 ]
