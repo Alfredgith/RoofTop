c7f4a4a22822d8616e71b6a388784e3ac7a4c58a
lenge <lengealfred@gmail.com>
2025-11-05 03:06:04 +0100

    fix error

--- mypro/mypro/wsgi.py
+++ mypro/mypro/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mypro.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mypro.mypro.settings')
 
 application = get_wsgi_application()
